7acf9088bc6e30d3a64243ac8b27fa659ec6b021
znmop <zacharyprado1@gmail.com>
2019-10-21 09:59:58 -0700

    fixed semantic error in remove duplicate from sorted array

--- cpp/remove-duplicates-from-sorted-array.cpp
+++ cpp/remove-duplicates-from-sorted-array.cpp
@@ -8,25 +8,25 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         const int size = nums.size();
-        int duplicate_count = 0;
+        int new_length = 0;
 
         for (int i = 0; i < size; ++i) {
             if (i == 0 or nums[i] != nums[i-1]) {
-                nums[duplicate_count] = nums[i];
-                duplicate_count++;
+                nums[new_length] = nums[i];
+                new_length++;
             }
         }
-        return duplicate_count;
+        return new_length;
     }
 };
 
 
 
 int main() {
-    vector<int> vec = {1,1,2,3,4,5,6,6,6,7,8,8,8,8,8,8,8,9,9,9,9,9,9,9,9,9,10,10,10,10};
+    vector<int> vec = {0,0,1,1,1,2,2,3,3,4};
     Solution s;
-    int dupes = s.removeDuplicates(vec);
-    cout << "Duplicates removed: " << dupes << endl;
+    int new_len = s.removeDuplicates(vec);
+    cout << "length after duplicates removed: " << new_len << endl;
 
     // for (int num = 0; num < vec.size(); ++num) {
     //     cout << vec[num] << ", ";
