573f56233212fb52b682b6a5a74624dc52cdff11
znmop <zacharyprado1@gmail.com>
2019-11-18 09:12:19 -0800

    two sum revisited. brute force inplace is runs in quadratic time with constant space. map solution uses linear space but runs in linear time.

--- cpp/two-sum.cpp
+++ cpp/two-sum.cpp
@@ -1,3 +1,43 @@
+// quadratic runtime, inplace solution (brute force method)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        const int size = nums.size();
+        if (size == 2) {
+            return {0, 1};
+        }
+
+        for (int i = 0; i < size; ++i) {
+            for (int j = i + 1; j < size; ++j) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        assert(false);
+    }
+};
+
+
+// O(n) solution using a map and the distance from each sum
+// the map stores each array value as a key, and its index as a value.
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> distances;
+        for (int i = 0; i < nums.size(); ++i) {
+            int check = target - nums[i];
+            if (distances.find(check) != distances.end()) {
+                return {i, distances[check]};
+            }
+            distances[nums[i]] = i;
+        }
+        assert(false);
+    }
+};
+
+
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
--- py/two-sum.py
+++ py/two-sum.py
@@ -11,38 +11,34 @@ Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 '''
 # O(n^2) runtime solution, O(1) extra space
-class Solution:
+class Solution0:
     def twoSum(self, nums: [int], target: int) -> [int]:
         if len(nums) == 2:
             return [0, 1]
 
         for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
+            for j in range(i + j + 1, len(nums)):
+                check = nums[i] + nums[j]
+                if target == check:
                     return [i, j]
 
-
-# O(n) runtime+space solution, is the # of unique integers in nums
+# O(n) runtime+space solution, where n is the # of unique integers in nums
 class Solution:
     def twoSum(self, nums: [int], target: int) -> [int]:
-        # target -= 30
-        seen = {}
-        if len(nums) == 2:
-            return [0, 1]
+        distances = {}
 
-        ret = [None, None]
-        for key, val in enumerate(nums):
-            comp = target - val
-            if comp in seen:
-                return [seen[comp], key]
-            seen[val] = key
+        # preprocess, then check
+        for i, num in enumerate(nums):
+            if (target - num) in distances:
+                return [i, distances[target - num]]
+            distances[num] = i
 
 
 
 
 
-nums = [3,4,2]
-target = 6
+nums = [3,4,7]
+target = 11
 print(Solution().twoSum(nums, target))
 nums = [2,7,11,15]
 target = 9
